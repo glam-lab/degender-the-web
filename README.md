e9f17b50af46d3ebdd6b852e5675f55e81f11d2b
Janet Davis <davisj@whitman.edu>
2019-07-10 15:32:46 -0700

    Tooltips give the replaced text. Tom Maxwell is no longer acknowledged because his starter code is no longer evident here.

--- README.md
+++ README.md
@@ -11,5 +11,3 @@ Inspired by Jenna Glatzer's Facebook post of July 8, 2019:
 > "non-consensual sex" is polite weasel-wording for "rape," and rapists don't deserve your politeness. Their victims deserve validation of what they've been through.
 
 <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fjennaglatzer%2Fposts%2F10162051686575506&width=500" width="500" height="280" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
-
-Thanks to Tom Maxwell for providing template code and instructions: [How to: Make your own text-replacing Chrome Extension](https://9to5google.com/2015/06/14/how-to-make-a-chrome-extensions/)
--- content.js
+++ content.js
@@ -1,29 +1,33 @@
 var dictionary = { "underage woman" : "child",
-                  "child prostitute": "rape victim",
-                  "sex with minors": "rape",
-                  "sex with girls": "rape",
-                  "non-consensual sex": "rape"};
+                   //"underage women" : "children",
+                   "child prostitute": "rape victim",
+                   "sex with minors": "rape",
+                   "sex with children": "rape",
+                   "sex with girls": "rape",
+                   "non-consensual sex": "rape"};
 
-var elements = document.getElementsByTagName('*');
+// TreeWalker did not behave as I expected, so we'll do it with explicit recursion.
+function textNodesUnder(node){
+  var all = [];
+  for (node=node.firstChild;node;node=node.nextSibling){
+    if (node.nodeType==3) all.push(node);
+    else all = all.concat(textNodesUnder(node));
+  }
+  return all;
+}
 
-for (var i = 0; i < elements.length; i++) {
-    var element = elements[i];
+var textNodes = textNodesUnder(document.body);
 
-    for (var j = 0; j < element.childNodes.length; j++) {
-        var node = element.childNodes[j];
+for (node of textNodes) {
+    var text = node.nodeValue;
+    for (phraseToReplace in dictionary) {
+        var pattern = new RegExp('\\b(' + phraseToReplace + ')\\b', 'ig');
+        var replacement = '<span class="tooltip">' + dictionary[phraseToReplace] + 
+                              '<span class="tooltiptext">Was: "' + phraseToReplace + '"</span></span>';
+        var replacedText = text.replace(pattern, replacement);
 
-        if (node.nodeType === 3) {
-            var text = node.nodeValue;
-            var phraseToReplace;
-            for ( phraseToReplace in dictionary ) {
-                var pattern = new RegExp('\\b(' + phraseToReplace + ')\\b', 'ig');
-                var replacement = '<span style="color:red;">' + dictionary[phraseToReplace] + '</span>';
-                var replacedText = text.replace(pattern, replacement);
-
-                if (replacedText !== text) {
-                    element.innerHTML = replacedText;
-                }
-            }
+        if ((replacedText !== text) && (node.parentNode !== null)) {
+          node.parentNode.innerHTML = replacedText;
         }
     }
 }
