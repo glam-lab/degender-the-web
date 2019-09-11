e2c9ea4104a0995ec9fa8bdfdf596d81b1484164
Janet Davis <davisj@whitman.edu>
2019-09-11 14:56:00 +0100

    Move all code re: pronouns from word-replacement to pronoun-replacement

--- src/pronoun-replacement.js
+++ src/pronoun-replacement.js
@@ -1,3 +1,4 @@
+/*globals nlp */
 import {
     compoundPronouns,
     genderPronouns,
@@ -12,7 +13,7 @@ const regexp = new RegExp(
     "\\b(" + Object.keys(allPronouns).join("|") + ")\\b",
     "i"
 );
-function hasReplaceablePronouns(text) {
+export function hasReplaceablePronouns(text) {
     return regexp.test(text);
 }
 
@@ -25,15 +26,57 @@ function substitute(pronoun) {
     return result;
 }
 
+// Fix up possessive adjectives:
+// "her book" -> "them book" -> "their book"
+// "his book" -> "theirs book" -> "their book"
+function replacePossessiveAdjectives(text) {
+    // Spencer and I disagree about whether a semicolon or a dash is equivalent
+    // to a period. So we're going to force the issue.
+    if (text.includes(";")) {
+        return text
+            .split(";")
+            .map(t => replacePossessiveAdjectives(t))
+            .join("; ")
+            .replace(/ +/g, " ");
+    }
+    if (text.match(/ -+ /)) {
+        return text
+            .split(/ -+ /)
+            .map(t => replacePossessiveAdjectives(t))
+            .join(" - ")
+            .replace(/ +/g, " ");
+    }
+
+    const doc = nlp(text);
+    ["her", "his"].forEach(function(pronoun) {
+        doc.match("[" + pronoun + "] #Noun")
+            .not("#Acronym")
+            .forEach(function(m) {
+                const matchedWord = m
+                    .clone()
+                    .setPunctuation("")
+                    .trim()
+                    .out("text");
+                m.replaceWith(
+                    isCapitalized(matchedWord)
+                        ? substitute(capitalize(pronoun) + "_poss_adj")
+                        : substitute(pronoun + "_poss_adj")
+                );
+            });
+    });
+    return doc.all().out("text");
+}
+
 // Replace the pronouns in given text, expanding contractions.
 // If compound pronouns are found, do not look for singular gender pronouns.
-function replacePronouns(text) {
+export function replacePronouns(text) {
     let result = replaceWords(
         text,
         Object.keys(compoundPronouns),
         substitute,
         false
     );
+    result = replacePossessiveAdjectives(result);
     result = replaceWords(
         result,
         Object.keys(genderPronouns),
@@ -43,5 +86,3 @@ function replacePronouns(text) {
     result = pluralizeVerbs(result);
     return result;
 }
-
-export { hasReplaceablePronouns, replacePronouns };
--- src/word-replacement.js
+++ src/word-replacement.js
@@ -23,24 +23,7 @@ function replaceWithCapitalization(doc, substitute, word) {
         });
 }
 
-// Replace possessive adjectives, a special case.
-function replacePossessiveAdjective(doc, substitute, pronoun) {
-    const uc = substitute(capitalize(pronoun) + "_poss_adj");
-    const lc = substitute(pronoun + "_poss_adj");
-    doc.match("[" + pronoun + "] #Noun")
-        .not("#Acronym")
-        .forEach(function(m) {
-            const matchedWord = m
-                .clone()
-                .setPunctuation("")
-                .trim()
-                .out("text");
-            m.replaceWith(isCapitalized(matchedWord) ? uc : lc);
-        });
-}
-
 // Replace the words in given text using the given substitute function.
-// Preserve title and lower case, ignoring ACRONYMS.
 // An option is given to expand contractions.
 export function replaceWords(
     text,
@@ -48,23 +31,6 @@ export function replaceWords(
     substitute,
     expandContractions = false
 ) {
-    // Spencer and I disagree about whether a semicolon or a dash is equivalent
-    // to a period. So we're going to force the issue.
-    if (text.includes(";")) {
-        return text
-            .split(";")
-            .map(t => replaceWords(t, words, substitute, expandContractions))
-            .join("; ")
-            .replace(/ +/g, " ");
-    }
-    if (text.match(/ -+ /)) {
-        return text
-            .split(/ -+ /)
-            .map(t => replaceWords(t, words, substitute, expandContractions))
-            .join(" - ")
-            .replace(/ +/g, " ");
-    }
-
     const doc = nlp(text);
     if (expandContractions) {
         doc.contractions().expand();
@@ -72,15 +38,6 @@ export function replaceWords(
     let word = null;
     for (word of words) {
         if (doc.has(word)) {
-            if (word === "her") {
-                // First, "her #Noun" -> "their #Noun"
-                replacePossessiveAdjective(doc, substitute, "her");
-                // Otherwise, "her" -> "them"
-            } else if (word === "his") {
-                // First, "his #Noun" -> "their #Noun"
-                replacePossessiveAdjective(doc, substitute, "his");
-                // Otherwise "his" -> "theirs"
-            }
             replaceWithCapitalization(doc, substitute, word);
         }
     }
