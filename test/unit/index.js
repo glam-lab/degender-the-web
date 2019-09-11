919815b9d166981b639dad92eaf6392cca10e2f2
Janet Davis <davisj@whitman.edu>
2019-09-11 21:47:26 +0100

    Reorder tests to better reflect dependencies

--- test/unit/index.js
+++ test/unit/index.js
@@ -1,9 +1,11 @@
-import "./capitalization.test.js";
 import "./dom-construction.test.js";
 import "./dom-traversal.test.js";
+
 import "./excluded-domains.test.js";
-import "./pronoun-replacement.test.js";
 import "./stopword-highlights.test.js";
+
+import "./capitalization.test.js";
 import "./verb-pluralization.test.js";
 import "./word-replacement.test.js";
+import "./pronoun-replacement.test.js";
 import "./grammar/test-runner.js";
