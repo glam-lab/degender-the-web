47403b01f52868f53f9a49c76cbd7f3737b65a87
Ian Hawkins <ian@hawkapp.com>
2019-09-15 00:26:37 -0700

    Reproduce all DGtW header functionality in popup
    The "Dismiss this header" button is not included in the popup, for
    obvious reasons. The "Restore original content" button is fully working.
    The message remains the same after restoring content.

--- src/popup.js
+++ src/popup.js
@@ -1,9 +1,23 @@
 /*global chrome */
 
-chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, { type: "getStatus" }, function(
-        response
-    ) {
-        document.getElementById("status").innerHTML = response.statusText;
+function updateStatus() {
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, { type: "getStatus" }, function(
+            response
+        ) {
+            document.getElementById("status").innerHTML = response.statusText;
+        });
     });
-});
+}
+
+function restoreOriginalContent() {
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, { type: "restoreOriginalContent" });
+    });
+}
+
+document
+    .getElementById("restore")
+    .addEventListener("click", restoreOriginalContent);
+
+updateStatus();
