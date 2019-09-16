67ba0aca1c4083dd7178b7041284bb736dc490d3
Janet Davis <davisj@whitman.edu>
2019-09-16 14:48:08 +0100

    Renamed highlightTag per suggestion from @irhawk

--- test/unit/stopword-highlights.test.js
+++ test/unit/stopword-highlights.test.js
@@ -10,7 +10,7 @@ import {
 } from "../../src/stopword-highlights.js";
 
 const genderWords = ["gender", "transgender", "gender-normative", "Gender"];
-const highlightTag = "<strong";
+const highlightTagPattern = "<strong";
 
 describe("stopword-highlights.js", function() {
     describe("mentionsGender", function() {
@@ -34,7 +34,7 @@ describe("stopword-highlights.js", function() {
             it("should highlight the word '" + w + "'", function() {
                 const text = "blah blah " + w + " blah blah";
                 const result = highlightGender(text);
-                chai.expect(result).to.include(highlightTag);
+                chai.expect(result).to.include(highlightTagPattern);
             });
         });
     });
@@ -99,20 +99,20 @@ describe("stopword-highlights.js", function() {
         it("should highlight 'she/her'", function() {
             const text = "I use she/her pronouns.";
             chai.expect(highlightPersonalPronounSpecs(text)).to.include(
-                highlightTag
+                highlightTagPattern
             );
         });
         it("should highlight any listed personal pronoun spec", function() {
             personalPronounSpecs.forEach(function(pp) {
                 chai.expect(
                     highlightPersonalPronounSpecs("I use " + pp + " pronouns.")
-                ).to.include(highlightTag);
+                ).to.include(highlightTagPattern);
             });
         });
         it("should highlight multiple personal pronoun specs", function() {
             const text = "I use either they/them or ey/em.";
             const result = highlightPersonalPronounSpecs(text);
-            const re = new RegExp(highlightTag, "g");
+            const re = new RegExp(highlightTagPattern, "g");
             let count = 0;
             while (re.exec(result)) count++;
             chai.expect(count).to.equal(2);
