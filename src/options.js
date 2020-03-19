c7e07cb523045e2a453bd0a361ba01e015973bd4
Ian Hawkins <ian@hawkapp.com>
2020-03-19 15:56:50 -0700

    Replace options list textarea with new prototype
    The textarea is replaced with an unordered list and a text field for
    adding new items. The options page is unstyled.

--- src/options.js
+++ src/options.js
@@ -1,42 +1,82 @@
 /*global chrome */
-// Saves options to chrome.storage
-function save_options() {
-    let doNotReplaceList = document
-        .getElementById("doNotReplaceList")
-        .value.split("\n");
-    doNotReplaceList = doNotReplaceList.filter(Boolean); // Remove empty strings
-
-    chrome.storage.sync.set(
-        {
-            doNotReplaceList: doNotReplaceList
-        },
-        function() {
-            // Update status to let user know options were saved.
-            const status = document.getElementById("status");
-            status.classList.remove("hide");
-            status.classList.add("show");
-            setTimeout(function() {
-                status.classList.add("show");
-                status.classList.remove("hide");
-            }, 750);
-        }
-    );
-}
-
-// Restores select box and checkbox state using the preferences
-// stored in chrome.storage.
 function restore_options() {
     chrome.storage.sync.get(
         {
             doNotReplaceList: []
         },
         function(items) {
-            const doNotReplaceListText = items.doNotReplaceList.join("\n");
-            document.getElementById(
-                "doNotReplaceList"
-            ).value = doNotReplaceListText;
+            displayDoNotReplaceList(items.doNotReplaceList);
         }
     );
 }
+
+function addItem() {
+    const newEntry = document.getElementById("newEntry").value;
+    chrome.storage.sync.get(
+        {
+            doNotReplaceList: []
+        },
+        function(items) {
+            items.doNotReplaceList.push(newEntry);
+            items.doNotReplaceList.sort();
+            chrome.storage.sync.set(items);
+
+            // Update the displayed list
+            displayDoNotReplaceList(items.doNotReplaceList);
+        }
+    );
+}
+
+function removeItem(urlToRemove) {
+    chrome.storage.sync.get(
+        {
+            doNotReplaceList: []
+        },
+        function(items) {
+            items.doNotReplaceList.sort();
+            const index = items.doNotReplaceList.findIndex(function(s) {
+                return s === urlToRemove;
+            });
+
+            // If the item hasn't already been removed from the list,
+            if (index !== -1) {
+                items.doNotReplaceList.splice(index, 1);
+            }
+
+            chrome.storage.sync.set(items);
+
+            // Update the displayed list
+            displayDoNotReplaceList(items.doNotReplaceList);
+        }
+    );
+}
+
+function displayDoNotReplaceList(doNotReplaceList) {
+    const ul = document.getElementById("doNotReplaceList");
+
+    // Clear listed children
+    ul.innerHTML = "";
+
+    for (const url of doNotReplaceList) {
+        const div = document.createElement("li");
+        const deleteButton = document.createElement("span");
+        deleteButton.classList.add("delete");
+        deleteButton.appendChild(document.createTextNode("x"));
+        deleteButton.addEventListener("click", function() {
+            removeItem(
+                this.parentElement.querySelector(".url-li-label").innerText
+            );
+        });
+
+        const text = document.createElement("span");
+        text.classList.add("url-li-label");
+        text.appendChild(document.createTextNode(url));
+
+        div.appendChild(text);
+        div.appendChild(deleteButton);
+        ul.appendChild(div);
+    }
+}
+
 document.addEventListener("DOMContentLoaded", restore_options);
-document.getElementById("save").addEventListener("click", save_options);
+document.getElementById("add").addEventListener("click", addItem);
